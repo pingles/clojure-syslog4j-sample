0bae716ad7213281d9f4db9c04f0a6f288af3977
Paul Ingles <paul@oobaloo.co.uk>
2011-06-24 15:40:12 +0100

    Send a UDP message

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject clojure-syslog4j-sample "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.syslog4j/syslog4j "0.9.30"]]
+  :dev-dependencies [[swank-clojure "1.3.1"]])
--- src/clojure_syslog4j_sample/core.clj
+++ src/clojure_syslog4j_sample/core.clj
@@ -1,1 +1,6 @@
-(ns clojure-syslog4j-sample.core)
+(ns clojure-syslog4j-sample.core
+  (:import [org.productivity.java.syslog4j Syslog]))
+
+(defn send-message
+  [msg]
+  (.info (Syslog/getInstance "udp") msg))
